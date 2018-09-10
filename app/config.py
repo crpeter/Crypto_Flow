5bd151259ed6c8443742643960b3aee1e0cd59f8
Cody Peter <crpeter@iastate.edu>
2018-09-10 00:56:25 -0500

    gatherer.py

---
+++ app/config.py
@@ -0,0 +1,6 @@
+## configaliglig
+
+api_key = 'b5AVKh4m79EzCdIpUiburS4CtyRKlamA3x0kMz81Fges5LWqpS8rGZJgt4Kj81Un'
+api_secret = 'SiZKAPJNYabblnANENKH1JeBKbzSTfUvFaYiMQod89bpCjSruwtfdgjVmqL4eexo'
+
+recv_window = 6000000
